ad66efa797b696160dfad3ef5482295a8d2f015a
SpiDrone <brendenbrown78@gmail.com>
2026-09-21 21:10:50 -0400

    MCreator Merge commit '982a176ef8e90672e6ed3505a42dcabd4b000531' into c51d97d23318679e580721602414a604df93475a

--- src/main/java/net/spidrotech/duality/DualityMod.java
+++ src/main/java/net/spidrotech/duality/DualityMod.java
@@ -47,7 +47,6 @@ public class DualityMod {
 		DualityModParticleTypes.REGISTRY.register(modEventBus);
 		DualityModAttributes.REGISTRY.register(modEventBus);
 		// Start of user code block mod init
-		net.spidrotech.duality.abilities.demon.ShimmerEffects.REGISTRY.register(modEventBus);
 		// End of user code block mod init
 	}
 
